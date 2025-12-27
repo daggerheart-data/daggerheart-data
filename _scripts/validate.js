2752133dea4f82343fbc1d481cff5a153dcf46e3
Jordan Potter <me@jordanpotter.com>
2025-12-27 05:52:10 +0000

    Add schema validation script

--- _scripts/validate.js
+++ _scripts/validate.js
@@ -1,1 +1,33 @@
-console.log("TODO");
+const Ajv = require("ajv/dist/2020");
+const fs = require("fs");
+const path = require("path");
+const { globSync } = require("glob");
+
+const ajv = new Ajv({ allErrors: true });
+const schemas = new Map();
+
+for (const schemaPath of globSync("schemas/**/*.schema.json")) {
+  const schema = JSON.parse(fs.readFileSync(schemaPath, "utf8"));
+  schemas.set(schemaPath, schema);
+  ajv.addSchema(schema);
+}
+
+for (const dataPath of globSync("releases/**/*.json")) {
+  const data = JSON.parse(fs.readFileSync(dataPath, "utf8"));
+
+  const schemaPath = path.join(
+    "schemas",
+    path.basename(dataPath).replace(".json", ".schema.json")
+  );
+
+  if (!schemas.has(schemaPath)) {
+    console.error(`Missing schema for ${dataPath}`);
+    process.exit(1);
+  }
+
+  if (!ajv.validate(schemas.get(schemaPath), data)) {
+    console.error(`Failed validation for ${dataPath}`);
+    console.error(ajv.errors);
+    process.exit(1);
+  }
+}
